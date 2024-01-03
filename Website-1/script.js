637818f7eb5b2c3196fbccda233a2ffcda98b637
Ashutosh <vashutosh625@gmail.com>
2024-01-03 19:38:47 +0530

    added button hover animation

--- Website-1/script.js
+++ Website-1/script.js
@@ -1,7 +1,21 @@
 
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
 
+const project = document.querySelectorAll('.projects')
+const fixed = document.querySelector('#fixed-image');
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+for (let index = 0; index < project.length; index++) {
+    project[index].addEventListener('mouseenter', function(){
+        fixed.style.display = 'block';
+        fixed.style.backgroundImage = `url(${project[index].dataset.name})`
+
+    })
+    project[index].addEventListener('mouseleave', function(){
+        fixed.style.display = 'none';
+    })
+    
+}
+
