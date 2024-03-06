0aa189fd1c981aa269946fb145132bc2f156db38
Ashutosh <vashutosh625@gmail.com>
2024-03-06 23:41:46 +0530

    updated

--- Website-2/script.js
+++ Website-2/script.js
@@ -13,7 +13,6 @@ const cursorAnimation = () => {
     gsap.to(cursor, {
       opacity: 0,
       scale: 0,
-      
     });
   });
 
@@ -38,28 +37,37 @@ const cursorAnimation2 = () => {
 
   page5Content.addEventListener("mouseleave", function (event) {
     gsap.to(cursor, {
-      opacity:0,
-      // scale: 0,
+      opacity: 0,
+      scale: 0,
     });
   });
 
   page5Content.addEventListener("mouseenter", function (event) {
     gsap.to(cursor, {
       opacity: 1,
-      // scale: 1,
+      scale: 1,
     });
   });
 };
 
 const smoothScroll = () => {
   const lenis = new Lenis({
+    // wrapper: '#main',
     lerp: 0.06,
     smoothWheel: true,
   });
 
-  lenis.on("scroll", (e) => {
-    console.log(e);
+  // lenis.on("scroll", (e) => {
+  //   console.log(e);
+  // });
+
+  lenis.on("scroll", ScrollTrigger.update);
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
   });
+
+  gsap.ticker.lagSmoothing(0);
 
   function raf(time) {
     lenis.raf(time);
@@ -103,10 +111,49 @@ const underLineAnimation = (element, page) => {
   });
 };
 
+const tl = gsap.timeline();
+
+tl.from("#loader h3", {
+  x: 40,
+  opacity: 0,
+  duration: 2,
+  stagger: 0.1,
+});
+tl.to("#loader h3", {
+  opacity: 0,
+  x: -40,
+  duration: 1,
+  stagger: 0.1,
+});
+tl.to("#loader", {
+  duration: 1.2,
+  ease: "power4.inout",
+  height: 0,
+  // opacity: 0,
+});
+tl.from("#page1-content h1 span", {
+  delay: -1.5,
+  y: 200,
+  stagger: 0.15,
+  ease: "power4.inout",
+  duration: 1,
+});
+tl.to("#loader", {
+  display: "none",
+});
+tl.from("nav h3, nav h4", {
+  delay: -1.3,
+  duration: 2,
+  y: 40,
+  opacity: 0,
+  ease: "power4.out",
+});
+
 cursorAnimation();
 smoothScroll();
 splitTextAnimation(".page2-top-ani, .page2-bottom-ani", "#page2");
 splitTextAnimation(".page3-top-ani", "#page3");
 splitTextAnimation(".page4-top-ani, .page4-bottom-ani", "#page4");
+splitTextAnimation(".page6-top-ani, .page6-bottom-ani", "#page6");
 underLineAnimation(".page4-underline", "#page4");
 cursorAnimation2();
